faa2d2207af23e6fe579838e77d17e84849ec1f0
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-09-20 18:11:12 +0700

    feat: use routing for header

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import HeroSection from './components/layout/HeroSection'
 import MainLayout from './components/layout/MainLayout'
 import UserProvider from './components/context/UserProvider'
 import CourseDashBoard from './components/pages/CourseDashBoard'
-
+import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <UserProvider>
-        <MainLayout>
-          <HeroSection />
-          <CourseDashBoard />
-        </MainLayout>
-      </UserProvider>
+      <BrowserRouter>
+        <UserProvider>
+          <MainLayout>
+            <Routes>
+              <Route path="/" element={<HeroSection />} />
+              <Route path="/courses" element={<CourseDashBoard />} />
+          </Routes>
+          </MainLayout>
+        </UserProvider>
+      </BrowserRouter>
     </div>
   )
 }
